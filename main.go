5d5e7c90a2ebbb8ceede91ae5490188fce7cca27
Zac Garby <me@zacgarby.co.uk>
2021-10-01 17:24:04 +0100

    initial web server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	r := mux.NewRouter()
+
+	// api := r.PathPrefix("/api/").Subrouter()
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/index.html")
+	})
+
+	fmt.Println("listening on http://localhost:8000")
+	http.ListenAndServe(":8000", r)
 }
