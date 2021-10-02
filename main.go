b5fc66fea5328b2d4980bd1ae0627cb2e4f88d89
Zac Garby <me@zacgarby.co.uk>
2021-10-02 17:03:44 +0100

    implement /api/login

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/zac-garby/magicalinternetpoints/api"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
+
 	r := mux.NewRouter()
 
-	// api := r.PathPrefix("/api/").Subrouter()
+	if err := api.RegisterAPI(r); err != nil {
+		log.Fatalf("could not create API. %s", err)
+	}
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
@@ -18,10 +25,10 @@ func main() {
 		http.ServeFile(w, r, "./static/login.html")
 	})
 
-	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	r.Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/index.html")
 	})
 
-	fmt.Println("listening on http://localhost:8000")
+	log.Println("listening on http://localhost:8000")
 	http.ListenAndServe(":8000", r)
 }
