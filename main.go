0637259169ac55529f04b7825e5fec2d042deebc
Zac Garby <me@zacgarby.co.uk>
2021-10-07 18:20:42 +0200

    get https working

--- main.go
+++ main.go
@@ -8,11 +8,43 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/zac-garby/magicalinternetpoints/api"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
+	serv, m := makeHTTPSServer()
+
+	log.Println("listening on :80")
+	go http.ListenAndServe(":80", m.HTTPHandler(nil))
+
+	log.Println("listening on :443")
+	if err := serv.ListenAndServeTLS("", ""); err != nil {
+		log.Fatalf("ListenAndServeTLS() failed with %s\n", err)
+	}
+}
+
+func makeHTTPSServer() (*http.Server, *autocert.Manager) {
+	m := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("magicalinternetpoints.com", "www.magicalinternetpoints.com"),
+		Cache:      autocert.DirCache("secret"),
+	}
+
+	serv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      makeRouter(),
+		Addr:         ":443",
+		TLSConfig:    m.TLSConfig(),
+	}
+
+	return serv, m
+}
+
+func makeRouter() *mux.Router {
 	r := mux.NewRouter()
 
 	if err := api.RegisterAPI(r); err != nil {
@@ -29,6 +61,5 @@ func main() {
 		http.ServeFile(w, r, "./static/index.html")
 	})
 
-	log.Println("listening on http://localhost:8000")
-	http.ListenAndServe(":8000", r)
+	return r
 }
